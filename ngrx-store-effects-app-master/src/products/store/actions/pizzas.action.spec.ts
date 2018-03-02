c31792b2e17237ea3e4da6b193e767f8a359a27c
hariprasadkool <hariprasad.g@techpearl.com>
2018-03-02 18:17:26 +0530

    testing actions

---
+++ ngrx-store-effects-app-master/src/products/store/actions/pizzas.action.spec.ts
@@ -0,0 +1,171 @@
+import * as fromPizzas from './pizzas.action';
+
+describe('Pizzas Actions',()=>{
+
+    describe('LoadPizzas Actions',()=>{
+
+        describe('LoadPizzas',()=>{
+            it('should create an action',()=>{
+                const action = new fromPizzas.LoadPizzas();
+                expect({ ...action}).toEqual({
+                    type: fromPizzas.LOAD_PIZZAS,
+                }); 
+            })
+        })
+
+        describe('LoadPizzasFail',()=>{
+            it('should create an action',()=>{
+                const payload = {message: 'Load Error'};
+                const action = new fromPizzas.LoadPizzasFail(payload);
+                expect({ ...action}).toEqual({
+                    type: fromPizzas.LOAD_PIZZAS_FAIL,
+                }); 
+            })
+        })
+
+        describe('LoadPizzasSuccess',()=>{
+            it('should create an action',()=>{
+                const payload =  [
+                    {
+                      "id": 1,
+                      "name": "anchovy"
+                    },
+                    {
+                      "id": 2,
+                      "name": "bacon"
+                    },
+                    {
+                      "id": 3,
+                      "name": "basil"
+                    },
+                    {
+                      "id": 4,
+                      "name": "chili"
+                    },
+                    {
+                      "id": 5,
+                      "name": "mozzarella"
+                    },
+                    {
+                      "id": 6,
+                      "name": "mushroom"
+                    },
+                    {
+                      "id": 7,
+                      "name": "olive"
+                    },
+                    {
+                      "id": 8,
+                      "name": "onion"
+                    },
+                    {
+                      "id": 9,
+                      "name": "pepper"
+                    },
+                    {
+                      "id": 10,
+                      "name": "pepperoni"
+                    },
+                    {
+                      "id": 11,
+                      "name": "sweetcorn"
+                    },
+                    {
+                      "id": 12,
+                      "name": "tomato"
+                    }
+                  ],
+                  "pizzas": [
+                    {
+                      "name": "Blazin' Inferno",
+                      "toppings": [
+                        {
+                          "id": 10,
+                          "name": "pepperoni"
+                        },
+                        {
+                          "id": 9,
+                          "name": "pepper"
+                        },
+                        {
+                          "id": 3,
+                          "name": "basil"
+                        },
+                        {
+                          "id": 4,
+                          "name": "chili"
+                        },
+                        {
+                          "id": 7,
+                          "name": "olive"
+                        },
+                        {
+                          "id": 2,
+                          "name": "bacon"
+                        }
+                      ],
+                      "id": 1
+                    },
+                    {
+                      "name": "Seaside Surfin'",
+                      "toppings": [
+                        {
+                          "id": 6,
+                          "name": "mushroom"
+                        },
+                        {
+                          "id": 7,
+                          "name": "olive"
+                        },
+                        {
+                          "id": 2,
+                          "name": "bacon"
+                        },
+                        {
+                          "id": 3,
+                          "name": "basil"
+                        },
+                        {
+                          "id": 1,
+                          "name": "anchovy"
+                        },
+                        {
+                          "id": 8,
+                          "name": "onion"
+                        },
+                        {
+                          "id": 11,
+                          "name": "sweetcorn"
+                        },
+                        {
+                          "id": 9,
+                          "name": "pepper"
+                        },
+                        {
+                          "id": 5,
+                          "name": "mozzarella"
+                        }
+                      ],
+                      "id": 2
+                    },
+                    {
+                      "name": "Plain Ol' Pepperoni",
+                      "toppings": [
+                        {
+                          "id": 10,
+                          "name": "pepperoni"
+                        }
+                      ],
+                      "id": 3
+                    }
+                  ];
+                const action = new fromPizzas.LoadPizzasSuccess(payload);
+                expect({ ...action}).toEqual({
+                    type: fromPizzas.LOAD_PIZZAS_SUCCESS,
+                }); 
+            })
+        })
+
+        
+    })
+})
